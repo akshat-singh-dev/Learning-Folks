3f856d9c8c63141dede6b77bb54b8dd9e64ce66a
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-15 19:47:21 +0530

    Intro Logo & Sidebar

---
+++ index.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+    document.getElementById("myclass").style.display = "block";
+    document.querySelector('.logo').style.display = 'none';
+    console.log("Timeout works");
+}, 3000);
