b201325b6d17424163497cde43af2c267d306759
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-07-15 20:25:17 +0530

    Main

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 setTimeout(function () {
     document.getElementById("myclass").style.display = "block";
-    document.querySelector('.logo').style.display = 'none';
+    document.querySelector('#logo').style.display = 'none';
     console.log("Timeout works");
 }, 3000);
